0a1b319e3292531348449d924ef512ed5fb70d3d
janvi2021 <dalaljanvi20@gmail.com>
2022-06-21 20:23:50 +0530

    created pages and configured navigation lines

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('services',views.services,name='services'),
+    path('Contact',views.Contact,name='Contact'),
+
+
 ]
